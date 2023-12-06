bc5b9536d370f6cc37c8e7c63eb7169aba9b1a83
Zamponi Marco <32539635+ZamponiMarco@users.noreply.github.com>
2023-12-06 14:36:08 +0100

    [ADD] Added function for getting slow waves detection by yasa and slow waves phases signal

--- closedloop/util.py
+++ closedloop/util.py
@@ -1,14 +1,24 @@
+from typing import Tuple, List
+
 import numpy as np
 
-def find_value_indices(arr, target_value):
-    indices = np.where(arr == target_value)[0]
+
+def find_value_indices(signal: np.ndarray, target_value) -> List[Tuple[int, int]]:
+    """
+    Get the couple of indices of the signal having as value the target value
+
+    :param signal: signal to analyze
+    :param target_value: value we want to find in the signal
+    :return: list of couples of indices pointing to the start and the end of a continuous signal with the value
+    """
+    indices = np.where(signal == target_value)[0]
 
     nearby_indices = []
 
     start = 0
 
-    for i in range(len(indices)-1):
-        if indices[i+1] - indices[i] > 1:
+    for i in range(len(indices) - 1):
+        if indices[i + 1] - indices[i] > 1:
             nearby_indices.append((indices[start], indices[i] + 1))
             start = i + 1
 
