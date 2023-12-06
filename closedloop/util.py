5b10197b9c7a604f5a9127711250e921a1e1850c
Zamponi Marco <32539635+ZamponiMarco@users.noreply.github.com>
2023-12-06 10:06:15 +0100

    [ADD] First tests

---
+++ closedloop/util.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def find_value_indices(arr, target_value):
+    indices = np.where(arr == target_value)[0]
+
+    nearby_indices = []
+
+    start = 0
+
+    for i in range(len(indices)-1):
+        if indices[i+1] - indices[i] > 1:
+            nearby_indices.append((indices[start], indices[i] + 1))
+            start = i + 1
+
+    return nearby_indices
+
+def
