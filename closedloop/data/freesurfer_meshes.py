e9f09002c440f774a492665521807531f59c8bc1
stansstanman <ruggero.basanisi@gmail.com>
2024-04-15 10:26:14 +0200

    small update

--- closedloop/data/freesurfer_meshes.py
+++ closedloop/data/freesurfer_meshes.py
@@ -27,9 +27,9 @@ def compute_freesurfer(freesurfer_home, subjects_dir,
     # Write commands
     with open(cmd_file, 'w') as f:
         f.writelines("#!/bin/bash\n")
-        f.writelines('export SUBJECTS_DIR={0}\n'.format(subjects_dir))
         f.writelines('export FREESURFER_HOME={0}\n'.format(freesurfer_home))
         f.writelines('source $FREESURFER_HOME/SetUpFreeSurfer.sh\n')
+        f.writelines('export SUBJECTS_DIR={0}\n'.format(subjects_dir))
         f.writelines('recon-all -subjid {0} -i {1} -all -parallel -openmp {2}'
                      .format(subject_name, mri_fname, n_jobs))
     # Change bash file rights for execution
@@ -160,16 +160,19 @@ def compute_scalp_meshes(freesurfer_home, subjects_dir, subject_name):
 
 
 if __name__ == '__main__':
-    fs_home = '/usr/local/freesurfer/7.3.2'
-    sbj_dir = '/home/jerry/freesurfer/TweakDreams'
+    # fs_home = '/usr/local/freesurfer/7.3.2'
+    fs_home = '/usr/local/freesurfer/7.4.1'
+    # sbj_dir = '/home/jerry/freesurfer/TweakDreams'
+    sbj_dir = '/media/jerry/ruggero/tweakdreams/freesurfer'
     sbj = 'TD001'
     mri = '/media/jerry/ruggero/dataset_td02/mri/nifti/' + \
         'sub-td001_ses-d01_mri.nii'
+    # mri = '/media/jerry/ruggero/tweakdreams/freesurfer/TD001/sub-td001_ses-d01_mri.nii'
     n_jobs = -1
-    vep_dir = '/home/jerry/python_projects/public/VEP_atlas_shared'
+    # vep_dir = '/home/jerry/python_projects/public/VEP_atlas_shared'
 
-    # compute_freesurfer(fs_home, sbj_dir, sbj, mri, n_jobs)
-    compute_VEP(vep_dir, fs_home, sbj_dir, sbj)
+    compute_freesurfer(fs_home, sbj_dir, sbj, mri, n_jobs)
+    # compute_VEP(vep_dir, fs_home, sbj_dir, sbj)
     # compute_watershed_bem(fs_home, sbj_dir, sbj)
     # compute_scalp_meshes(fs_home, sbj_dir, sbj)
     'mri_watershed -useSRAS -atlas -surf $SUBJECTS_DIR/$SUBJECT/bem/watershed $SUBJECTS_DIR/$SUBJECT/mri/T1.mgz $SUBJECTS_DIR/$SUBJECT/bem/TD001-head.fif'
